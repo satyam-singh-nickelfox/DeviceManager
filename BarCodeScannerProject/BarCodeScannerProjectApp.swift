bf96d855beb0ed593b9f2cfd6bdecc9dd55f7e3e
Satyam Singh <satyam.singh@nickelfox.com>
2024-07-09 14:58:07 +0530

    Initial Commit

---
+++ BarCodeScannerProject/BarCodeScannerProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerProjectApp.swift
+//  BarCodeScannerProject
+//
+//  Created by Satyam Singh on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
